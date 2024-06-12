829b7eb0253a8c31fb31f8e0ab7500681742af69
G3orgex <jorgecabutn75422@gmail.com>
2024-06-12 17:27:08 -0300

    agragacion de listado

--- 04-Gif-Expert-App/src/GifExpertApp.jsx
+++ 04-Gif-Expert-App/src/GifExpertApp.jsx
@@ -1,9 +1,23 @@
-import React from "react";
+import React,{useState} from "react";
 
 export const GifExpertApp = () => {
+   
+    const [categories, setCategories] = useState(['One Punch','DragonBall']);
+    
+    const onAddCategory = ()=>{}
   return (
-    <>
+    <>    
+    {/* Titulo */}
       <h1>GifExpertApp</h1>
+ {/* Input */}
+ {/* Listado de Gif */}
+ <button>Agregar</button>
+      <ol>
+        {categories.map( category =>{
+            return <li key={category}>{category}</li>
+        } )}
+        {/* <li></li> */}
+      </ol>
     </>
   );
 };
